92b2c4e8a27fd739981e2e91a7604e8bc58deae1
Yevhen Shepel <yevhen@Yevhens-Laptop.local>
2024-08-06 14:37:37 +0300

    change colors

--- App.tsx
+++ App.tsx
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {
+    useCallback,
+    useState,
+} from "react";
+
+import {
+    StyleSheet,
+    Text,
+    TextStyle,
+    Pressable,
+    ViewStyle,
+} from 'react-native';
+import { getRandomColor } from "./utils/get-random-color";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [viewStyle, setViewStyle] = useState<ViewStyle>(styles.container);
+    const [textStyle, setTextStyle] = useState<TextStyle>(styles.text);
+
+    const handlePress = useCallback(
+        () => {
+            const red = getRandomColor();
+            const green = getRandomColor();
+            const blue = getRandomColor();
+            setViewStyle({
+                ...styles.container,
+                backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+            });
+            setTextStyle({
+                ...styles.text,
+                color: `rgb(${255 - red}, ${255 - green}, ${255 - blue})`,
+            });
+        },
+        [],
+    );
+
+    return (
+        <Pressable
+            style={viewStyle}
+            onPress={handlePress}
+        >
+            <Text style={textStyle}>Hello there</Text>
+        </Pressable>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ffffff'
+    },
+    text: {
+        fontSize: 32,
+        lineHeight: 40,
+        color: '#000000',
+    },
 });
